3487b57002bc67cc6ab9e5d9e788f8c01da4c423
BrandonVielmas <abrahamvielmas1@gmail.com>
2023-11-15 11:00:02 -0600

    Agregamos nuestros ingresos

--- src/app/modules/dashboard/components/home/home.component.ts
+++ src/app/modules/dashboard/components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { IncomeService } from 'src/app/services/income.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -7,18 +9,59 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent  implements OnInit {
+  @ViewChild(IonModal) modal: any;
 
   public user: any;
 
-  constructor(private _userService: UserService) { }
+  constructor(private _userService: UserService, private _incomeService: IncomeService) { }
 
   ngOnInit() {
 
-    this._userService.getUserAccount(1).subscribe(res => {
-      this.user = res;
-      console.log(this.user);
-    })
+    this.getInfoUser()
 
   }
+
+  private getInfoUser() {
+    this._userService.getUserAccount(1).subscribe(res => {
+      this.user = res;
+    })
+  }
+
+  public handleCard() {
+    
+  }
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(null, 'confirm');
+  }
+
+  handleInput(e: any) {
+    let ingreso = parseFloat(e.target.value);
+    if(!isNaN(ingreso) && ingreso > 0) {
+      this.guardarIngreso(ingreso, this.user.userId)
+    }
+    
+  }
+
+  // onWillDismiss(e: any) {
+  //   let ingreso = parseFloat(e.target.value);
+  //   console.log(e.detail)
+  //   if(!isNaN(ingreso) && ingreso > 0 && e.detail.role === 'confirm') {
+  //     this.guardarIngreso(ingreso, this.user.userId)
+  //   }
+  // }
+
+  private guardarIngreso(ingreso: number, userId: number) {
+    this._incomeService.saveIncomeUser(ingreso, userId).subscribe(res => {
+      if(res) {
+        this.getInfoUser()
+      }
+    })
+  }
+
 
 }
